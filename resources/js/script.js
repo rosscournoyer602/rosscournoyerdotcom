c17f8eb887f5f5b2c182cd98ac9496dcea5ef7dd
Ross Cournoyer <ross.cournoyer@it-consultis.com>
2019-07-16 16:17:59 +0800

    Start updating project section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
-
-   //nav background change
-    $('.js--learning-section').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('grey-bg');
-        }
-        else {
-            $('nav').removeClass('grey-bg');
-        }
-    }, {
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  // nav background change
+  $('.js--learning-section').waypoint(
+    direction => {
+      if (direction === 'down') {
+        $('nav').addClass('grey-bg');
+      } else {
+        $('nav').removeClass('grey-bg');
+      }
+    },
+    {
       offset: '200px'
-    });
+    }
+  );
 
-    //smooth scrolling
-    $('.js--scroll-to-header').click(function() {
-        console.log('scroll to header')
-        $('html, body').animate({scrollTop: $('.js--header-section').offset().top}, 1000);
-    })
-    $('.js--scroll-to-bio').click(function() {
-        $('html, body').animate({scrollTop: $('.js--bio-section').offset().top}, 1000);
-    })
-    $('.js--scroll-to-projects').click(function() {
-        $('html, body').animate({scrollTop: $('.js--projects-section').offset().top}, 1000);
-    })
-    $('.js--scroll-to-learning').click(function() {
-        $('html, body').animate({scrollTop: $('.js--learning-section').offset().top}, 1000);
-    })
-    $('.js--scroll-to-contact').click(function() {
-        $('html, body').animate({scrollTop: $('.js--contact-section').offset().top}, 1000);
-    })
-
-
+  // smooth scrolling
+  $('.js--scroll-to-header').click(() => {
+    $('html, body').animate({ scrollTop: $('.js--header-section').offset().top }, 1000);
+  });
+  $('.js--scroll-to-bio').click(() => {
+    $('html, body').animate({ scrollTop: $('.js--bio-section').offset().top }, 1000);
+  });
+  $('.js--scroll-to-projects').click(() => {
+    $('html, body').animate({ scrollTop: $('.js--projects-section').offset().top }, 1000);
+  });
+  $('.js--scroll-to-learning').click(() => {
+    $('html, body').animate({ scrollTop: $('.js--learning-section').offset().top }, 1000);
+  });
+  $('.js--scroll-to-contact').click(() => {
+    $('html, body').animate({ scrollTop: $('.js--contact-section').offset().top }, 1000);
+  });
 });
